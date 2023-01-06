d8e1d39337482375f1a9a4e6e914e7a4687c3779
maximilian012 <kgw96@naver.com>
2023-01-06 14:31:36 +0900

    workproject

---
+++ src/MainClass.java
@@ -0,0 +1,50 @@
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		//System.out.println("hihihihi"); // ln = 줄바꿈 엔터
+		
+		// 한줄 주석 이줄은 컴파일되지 않는다
+		
+		/*
+		 * 
+		 * 범위주석
+		 * Html <!-- > 
+		 * 
+		 * JSP <%-- --%>
+		 * 
+		 * mySQL -- 
+		 * 
+		 *python # """ 주석문 """ 
+		 *
+		 */
+//		System.out.println("hell yeah");
+//		System.out.print("Hello");
+//		System.out.println("hell yeah");
+//		
+//		// 기능 : escape sequence 탈출 질의 
+//		// \n 개행, \b(backspace) , \": 따움표 같이 , \t (띄어쓰기), \\ -> 역슬래쉬 하나 
+//		System.out.print("World\n");
+//		System.out.print("헬로\b우");
+//		
+//		System.out.println("\"Hello Wolrd\"");
+//		
+//		System.out.println("\tHello\tWorld");
+////		System.out.println("\t God\t Did");
+////		System.out.println("\tGod\tDid");
+////		
+//		System.out.println("\\헬로우 월드\\");
+//		
+		System.out.printf("%s \n", "hihihi"); // Hello == 문자열 (String)
+		//  				표현자료형, 값 
+		System.out.println(88);
+		
+		System.out.println("Hello" + "\tBro");
+		
+		
+		
+		// print는 출력용도로 쓰임
+	}
+
+}
